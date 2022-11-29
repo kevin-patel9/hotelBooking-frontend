ab13fde7038458bfc75b3bb8dea3f3aeb702dfa5
Kevin Patel <kevinpatel0129@gmail.com>
2022-11-29 23:03:57 +0530

    link changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,7 +25,7 @@ export const Navbar = () => {
         <div className="loggedUser">
           <span>{}</span>
             <h4 style={{margin: "0", fontSize: "1.4rem", fontWeight: "normal" }}>{auth.user.username}</h4>
-            <a href="https://jolly-mochi-eba040.netlify.app" className="adminBtn">Admin Login</a>
+            <a href="https://steady-palmier-7e41cc.netlify.app" className="adminBtn">Admin Login</a>
             <button onClick={handleLogout} className="btn" >Logout</button>
         </div>
         </>
